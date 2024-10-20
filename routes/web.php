24dee89e5abd0073006756c8db7fd8f6be9cbaa9
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-10-20 12:48:45 +0200

    created BookController; added functions scopeMinReviews and dateRangeFilter into Book Model

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-
-    $books = \App\Models\Book::limit(5)->withAvg('reviews', 'rating')->orderBy('reviews_avg_rating')->get();
-
-    dd($books);
-    // return view('welcome');
+    return view('welcome');
 });
